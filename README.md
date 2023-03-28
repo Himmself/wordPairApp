c9d88feec3d9f35fc73f613d7b0ac48a61ee49ba
Himmself <37225647+Himmself@users.noreply.github.com>
2023-03-28 08:09:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wordPairApp
+ An app that you can like and unlike random wordPairs. Practice with Flutter
---
+++ android/app/src/main/kotlin/net/flatcap/cool_beans/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.flatcap.cool_beans
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
